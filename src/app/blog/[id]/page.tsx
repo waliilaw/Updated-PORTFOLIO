e0060a2ae57f47431a11fae38338fd4a1908a986
Wali <humaidwali20@gmail.com>
2025-02-19 21:15:47 +0530

    Add files via upload

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -3,6 +3,7 @@ import { useParams } from "next/navigation";
 import { AuroraBackground } from "@/components/ui/aurora-background";
 import { Navbar } from "@/components/Navbar";
 import { Back } from "@/components/Back";
+import Image from 'next/image';
 
 export default function BlogPost() {
   const params = useParams();
@@ -14,10 +15,12 @@ export default function BlogPost() {
       <Back />
       <div className="flex flex-col items-center justify-center min-h-screen gap-8">
         <h1 className="text-4xl text-white font-bold">Blog post not found</h1>
-        <img 
-          src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcDd6bWx4Y2E2OGF1OWF4OWF4cmRxdWR2dDU4NHB1bGx6YnB1dG42eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/14uQ3cOFteDaU/giphy.gif" 
+        <Image 
+          src="/no.gif"
           alt="Not Found" 
-          className="rounded-lg shadow-lg w-96 h-auto"
+          width={384}
+          height={384}
+          className="rounded-lg shadow-lg"
         />
       </div>
     </AuroraBackground>

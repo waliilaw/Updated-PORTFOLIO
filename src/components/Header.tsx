45610035eb65a459e625f4cc8d73aeeefa1c2273
Wali <humaidwali20@gmail.com>
2025-02-19 21:29:53 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export function Header() {
   return (
     <div className="flex justify-center items-center pr- pl-0">
       <div className="text-6xl pt-28 mx-auto font-bold">
-        <span className="inline-block text-6xl font-bold text-white">Hi! I'm Wali</span>
+        <span className="inline-block text-6xl font-bold text-white">Hi! I&apos;m Wali</span>
         <br></br>
 
         <span className="flex justify-center items-center text-6xl font-bold text-white p-6">

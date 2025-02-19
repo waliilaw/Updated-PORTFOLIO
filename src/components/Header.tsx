57f8d898fbd09ca4f7ccb696c85788b24e2454a6
Wali <humaidwali20@gmail.com>
2025-02-19 22:31:21 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { memo } from "react";
 import { FlipWords } from "./ui/flip-words";
 
-export function Header() {
+export const Header = memo(function Header() {
   const words = ["Full Stack Dev", "DSA & Leetcode" , "Web3 Enthusiast"];
 
   return (
     <div className="flex justify-center items-center pr- pl-0">
       <div className="text-6xl pt-28 mx-auto font-bold">
-        <span className="inline-block text-6xl font-bold text-white">Hi! I&apos;m Wali</span>
-        <br></br>
-
+        <span className="inline-block text-6xl font-bold text-white">
+          Hi! I&apos;m Wali
+        </span>
+        <br />
         <span className="flex justify-center items-center text-6xl font-bold text-white p-6">
           <FlipWords words={words} />
         </span>
@@ -17,4 +18,4 @@ export function Header() {
       </div>
     </div>
   );
-}
+});
